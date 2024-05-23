3f9bf4f22ac652b31a3f84255cbf8eb81092fe4d
Ojasvi Dev Tamang <lalamdorje@gmail.com>
2024-05-23 11:08:05 +0545

    Refractor[Layout]:Adds change on title and footer.

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="footer-content">
             <div class="row">
                 <div class="col-lg-8 footer-left">
-                    <p class="m-0">Copyright © <script>document.write(new Date().getFullYear())</script>. Design by <a href="{{route('frontend.index')}}">{{$siteSetting->name}}</a></p>
+                    <p class="m-0">Copyright © <script>document.write(new Date().getFullYear())</script>. Developed by <a href="{{route('frontend.index')}}">{{$siteSetting->name}}</a></p>
                 </div>
                 <div class="col-lg-4 footer-right text-lg-right text-center mt-lg-0 mt-3">
                     <ul class="social m-0 p-0">
